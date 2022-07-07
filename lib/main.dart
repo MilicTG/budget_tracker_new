48cbb924a2e852504921f8a2e353ae14fa6e1aad
MilicTG <milic.tg@gmail.com>
2022-07-07 17:43:24 +0200

    Chapter 7.5

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          colorScheme: ColorScheme.fromSeed(
+            brightness: Brightness.dark,
+            seedColor: Colors.indigo,
+          ),
         ),
         home: const Home());
   }
