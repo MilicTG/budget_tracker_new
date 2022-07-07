f16941f2f755fe950f4eaee3aa487f748fd36345
MilicTG <milic.tg@gmail.com>
2022-07-07 18:07:51 +0200

    Chapter 7.6

---
+++ lib/model/transaction_item.dart
@@ -0,0 +1,8 @@
+class TransactionItem {
+  String itemTitle;
+  double amount;
+  bool isExpense;
+
+  TransactionItem(
+      {required this.itemTitle, required this.amount, this.isExpense = true});
+}
